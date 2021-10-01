7c4f3fea8ce440d3e9361b6023818370c06a69aa
Dita indriawati <80086227+dtindriaa@users.noreply.github.com>
2021-10-01 14:24:04 +0700

    Add files via upload

---
+++ OperatorTernaryNo1.java
@@ -0,0 +1,18 @@
+package ditaindriawati;
+
+/**
+ *
+ * @author Dita Indriawati
+ */
+public class OperatorTernaryNo1 {
+    
+    public static void main(String[] args) {
+        
+        int number1 = 50000;
+        int number2 = 55000;
+        
+        String jawab = number1 > number2 ? "Membeli":"Tidak Membeli";
+        
+        System.out.println(jawab);
+    }
+}
